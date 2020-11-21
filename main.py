34f7e38ae3ccf58acd53ffdd821cc004fa2ac909
Bradley Thompson <bradlet2@pdx.edu>
2020-11-21 10:57:50 -0800

    Starter code

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # CS 545 - Anthony Rhodes
 
 import os
+import numpy as np
+
+def load_data(name):
+    return np.loadtxt(fname=name, delimiter=",", dtype=np.dtype(np.uint8))
 
 if __name__ == '__main__':
     os.chdir('./dataset')
