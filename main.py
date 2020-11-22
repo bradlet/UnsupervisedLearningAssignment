03c9a8dcd191129e188b83168409a1b580af5360
Bradley Thompson <bradlet2@pdx.edu>
2020-11-21 18:39:51 -0800

    Create KMeansLearner - doesn't run enough times

--- main.py
+++ main.py
@@ -5,6 +5,89 @@
 import os
 import numpy as np
 
+K_CLUSTERS = 10
+ITERATIONS = 10
+
+
+class KMeansLearner:
+
+    def __init__(self, data):
+        self.centroids = self.random_centroid_init(data)
+        self.clusters = create_array_of_empty_lists(K_CLUSTERS)
+
+    # Returns an array of indices for n centroids at random from data_table, where n = K_CLUSTERS
+    @staticmethod
+    def random_centroid_init(data):
+        random_indices = np.random.randint(0, len(data), K_CLUSTERS)
+        centroids = list()
+        for i in range(0, len(random_indices)):
+            centroids.append(data[random_indices[i]])
+        return centroids
+
+    # For each point in data, find which centroid is closest - argmin for distance()
+    # Assign point to that centroids set.
+    def assignment(self, data):
+        for point in data:
+            min_distance = np.inf
+            min_cluster = 0
+            for i in range(0, K_CLUSTERS):
+                current_distance = distance(self.centroids[i], point)
+                if current_distance < min_distance:
+                    min_distance = current_distance
+                    min_cluster = i
+            self.clusters[min_cluster].append(point)
+
+    # Find the new mean point for each cluster and set that to the centroids
+    def update(self):
+        for i in range(0, K_CLUSTERS):
+            self.centroids[i] = mean_point(self.clusters[i])
+
+    def calculate_cluster_error(self, cluster_number):
+        cluster_squared_error = 0
+        for point in self.clusters[cluster_number]:
+            cluster_squared_error += np.square(distance(point, self.centroids[cluster_number]))
+        return cluster_squared_error
+
+    def calculate_error(self):
+        sum_squared_error = 0
+        for i in range(0, K_CLUSTERS):
+            sum_squared_error += self.calculate_cluster_error(i)
+        return sum_squared_error
+
+    # Run the assignment and update steps n times, where n = ITERATIONS
+    #   For each step, record the sum squared error and centroids for this step.
+    def run_k_means(self, data):
+        run_history = list()
+        for i in range(0, ITERATIONS):
+            self.assignment(data)
+            self.update()
+            run_history = (self.calculate_error(), self.centroids)
+            self.clusters = create_array_of_empty_lists(K_CLUSTERS)
+        return run_history
+
+
+def create_array_of_empty_lists(size):
+    array = np.empty(size, type(list))
+    for i in range(0, size):
+        array[i] = list()
+    return array
+
+
+# Returns euclidean distance between two points
+# Note: both point args should be np vectors of len 2 b/c they implement operator -
+def distance(point1, point2):
+    # np.linalg.norm uses l2 norm by default!
+    return np.linalg.norm(point2 - point1)
+
+
+# Returns the point representing the mean
+def mean_point(list_of_points):
+    sum_x = sum_y = 0
+    for point in list_of_points:
+        sum_x += point[0]
+        sum_y += point[1]
+    return sum_x / len(list_of_points), sum_y / len(list_of_points)
+
 
 def load_data(name):
     return np.loadtxt(fname=name, dtype=np.dtype(np.float), usecols=range(2))
@@ -14,4 +97,12 @@ if __name__ == '__main__':
     os.chdir('./dataset')
     dataset = load_data('545_cluster_dataset.txt')
 
-    print(dataset[0])
+    learner = KMeansLearner(dataset)
+
+    runs = learner.run_k_means(dataset)
+    print(len(runs))
+    for run in runs:
+        print(run)
+    # FOR EACH POINT
+    #   Assign to cluster such that it is in the cluster who's euclidean distance to the mean is closest
+    # Then update means by taking the average value of all points in a cluster.
